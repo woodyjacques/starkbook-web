b5863e21b59505fee18fcdf73dfb01a8dea098d7
woodyjacques <Woodyjacques1@gmail.com>
2024-03-23 22:35:23 -0400

    Arreglando el header

--- src/components/headerSesion.tsx
+++ src/components/headerSesion.tsx
@@ -27,8 +27,7 @@ function Header() {
                     <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">StarkBook</span>
                 </a>
                 <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                    <a href="/starkbook-sesion
-                    "><button onClick={showModal}  type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Salir</button></a>
+                    <a href="#"><button onClick={showModal}  type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Salir</button></a>
                 </div>
             </div>
         </nav>
