0bb3c135860d6938b752d899ba04e448884c6460
woodyjacques <Woodyjacques1@gmail.com>
2024-03-29 15:28:07 -0400

    Arreglando el boton ver

--- src/components/CardsBook.tsx
+++ src/components/CardsBook.tsx
@@ -173,12 +173,13 @@ function CardsBook() {
                     </div>
                   ) : (
                     <div
-                      className="cursor-pointer text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-green-600 hover:bg-green-700 focus:ring-green-800"
+                      className="cursor-pointer text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-green-700 focus:ring-green-800"
                     >
                       Ver
                     </div>
                   )}
-                  
+
+
                 </div>
               </div>
             </div>
